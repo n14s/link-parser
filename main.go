679251a0fdbce477cfd4f9109c5fda074ce737f1
Niklas Mertens <niklas@competition.company>
2024-05-27 22:15:49 +0200

    feat: finish simple link parser

--- main.go
+++ main.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 	"io"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
-var htmlString = `
+var htmlExample = `
 <html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 <body>
-  <h1>Hello!</h1>
-  <a href="/other-page">A link to another page</a>
+  <h1>Social stuffs</h1>
+  <div>
+    <a href="https://www.twitter.com/joncalhoun">
+      Check me out on twitter
+      <i class="fa fa-twitter" aria-hidden="true">watup</i>
+    </a>
+    <a href="https://github.com/gophercises">
+      Gophercises is on <strong>Github</strong>!
+    </a>
+  </div>
 </body>
 </html>
 `
 
 func main() {
-	r := strings.NewReader(htmlString)
+	r := strings.NewReader(htmlExample)
 	links, err := Parse(r)
 	if err != nil {
 		panic(err)
@@ -32,7 +45,65 @@ type Link struct {
 	Text string
 }
 
+// Event represents a public event
+type Event struct {
+	Venue       string
+	Date        string
+	Description string
+}
+
 // Parse will take in an HTML document and will return a slice of links
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+
+	var links []Link
+	for _, n := range nodes {
+		links = append(links, buildLink(n))
+	}
+
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var link Link
+	for _, a := range n.Attr {
+		if a.Key == "href" {
+			link.Href = a.Val
+			break
+		}
+	}
+
+	link.Text = extractText(n)
+
+	return link
+}
+
+func extractText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var text string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text = text + " " + extractText(c)
+	}
+	formattedText := strings.Join(strings.Fields(text), " ")
+	return formattedText
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var nodes []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		nodes = append(nodes, linkNodes(c)...)
+	}
+	return nodes
 }
